d0e39664d1640413a9cfc6abcb5ece5e914031cc
Brandon <bsquared18@gmail.com>
2017-06-27 18:36:04 -0400

    Add js folder & contents

---
+++ js/nav.js
@@ -0,0 +1,25 @@
+$('.fa-bars').click(function() {
+  $('.tabs').toggle();
+});
+
+// Show main
+$('.tabs-about').click(function() {
+  $('.main-about').css('display', 'block');
+  $('.main-portfolio').css('display', 'none');
+  $('.main-contact').css('display', 'none');
+});
+
+
+//Show Portfolio
+$('.tabs-portfolio').click(function() {
+  $('.main-about').css('display', 'none');
+  $('.main-portfolio').css('display', 'block');
+  $('.main-contact').css('display', 'none');
+});
+
+// Show Contact
+$('.tabs-contact').click(function() {
+  $('.main-about').css('display', 'none');
+  $('.main-portfolio').css('display', 'none');
+  $('.main-contact').css('display', 'block');
+});
