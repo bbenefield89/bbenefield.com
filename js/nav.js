4a782470940d46a6608868926d0e32ac242e75f2
Brandon <bsquared18@gmail.com>
2017-06-27 23:54:19 -0400

    Wrap contents in function

--- js/nav.js
+++ js/nav.js
@@ -1,25 +1,27 @@
-$('.fa-bars').click(function() {
-  $('.tabs').toggle();
-});
+$(document).ready(function() {
+  $('.fa-bars').click(function() {
+    $('.tabs').toggle();
+  });
 
-// Show main
-$('.tabs-about').click(function() {
-  $('.main-about').css('display', 'block');
-  $('.main-portfolio').css('display', 'none');
-  $('.main-contact').css('display', 'none');
-});
+  // Show main
+  $('.tabs-about').click(function() {
+    $('.main-about').css('display', 'block');
+    $('.main-portfolio').css('display', 'none');
+    $('.main-contact').css('display', 'none');
+  });
 
 
-//Show Portfolio
-$('.tabs-portfolio').click(function() {
-  $('.main-about').css('display', 'none');
-  $('.main-portfolio').css('display', 'block');
-  $('.main-contact').css('display', 'none');
-});
+  //Show Portfolio
+  $('.tabs-portfolio').click(function() {
+    $('.main-about').css('display', 'none');
+    $('.main-portfolio').css('display', 'block');
+    $('.main-contact').css('display', 'none');
+  });
 
-// Show Contact
-$('.tabs-contact').click(function() {
-  $('.main-about').css('display', 'none');
-  $('.main-portfolio').css('display', 'none');
-  $('.main-contact').css('display', 'block');
+  // Show Contact
+  $('.tabs-contact').click(function() {
+    $('.main-about').css('display', 'none');
+    $('.main-portfolio').css('display', 'none');
+    $('.main-contact').css('display', 'block');
+  });
 });
