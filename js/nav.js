029533898cade96fc08aa94f1d69fafadd365166
Brandon <bsquared18@gmail.com>
2017-07-19 16:29:35 -0400

    Navbar

--- js/nav.js
+++ js/nav.js
@@ -1,27 +1,3 @@
-$(document).ready(function() {
-  $('.fa-bars').click(function() {
-    $('.tabs').toggle('slow');
-  });
-
-  // Show main
-  $('.tabs-about').click(function() {
-    $('.main-about').css('display', 'block');
-    $('.main-portfolio').css('display', 'none');
-    $('.main-contact').css('display', 'none');
-  });
-
-
-  //Show Portfolio
-  $('.tabs-portfolio').click(function() {
-    $('.main-about').css('display', 'none');
-    $('.main-portfolio').css('display', 'block');
-    $('.main-contact').css('display', 'none');
-  });
-
-  // Show Contact
-  $('.tabs-contact').click(function() {
-    $('.main-about').css('display', 'none');
-    $('.main-portfolio').css('display', 'none');
-    $('.main-contact').css('display', 'block');
-  });
+$('.fa-bars').click(function() {
+  $('#nav-links').toggle('slow');
 });
